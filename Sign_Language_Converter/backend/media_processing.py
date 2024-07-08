cf280dd378be4b8b391f291c5e6dceb32db6a80a
Krishna Paliwal <krishna.plwl264@gmail.com>
2024-07-08 19:11:59 +0530

    Made some improvements in the organisation of directorries and dockerized the project.

--- Sign_Language_Converter/backend/media_processing.py
+++ Sign_Language_Converter/backend/media_processing.py
@@ -1,36 +1,29 @@
 import cv2
 from ultralytics import YOLO
 
-# Load the YOLOv8 model
 model = YOLO('backend/weights.pt')
 
 # Process Image
 def convert_image_to_text(image_path):
     try:
-        # Read the image
+
         image = cv2.imread(image_path)
 
-        # Check if the image is loaded correctly
         if image is None:
             return "Could not load image... Image not found...", []
 
-        # Perform object detection on the image
         results = model(image)
 
-        # Define predicted label list and annotations
         pred_label_list = []
         annotations = []
 
-        # Process the results
         for result in results:
             for box in result.boxes:
                 class_label = model.names[int(box.cls)]
                 pred_label_list.append(class_label)
 
-                # Draw bounding box
                 xmin, ymin, xmax, ymax = map(int, box.xyxy[0])
 
-                # Save the annotation in the annotation dict
                 annotations.append({
                     "class_label": class_label,
                     "xmin": xmin,
@@ -39,7 +32,6 @@ def convert_image_to_text(image_path):
                     "ymax": ymax
                 })
 
-        # Return the predicted labels as a concatenated string and annotations
         return ' '.join(pred_label_list), annotations
     except Exception as e:
         return f"Error processing image: {str(e)}", []
@@ -47,34 +39,27 @@ def convert_image_to_text(image_path):
 # Process Video
 def convert_video_to_text(video_path):
     try:
-        # Open the video file
+
         cap = cv2.VideoCapture(video_path)
 
-        # Check if the video file is opened correctly
         if not cap.isOpened():
             return "Error: Could not open video file.", []
 
-        # Total Frames
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        # Text storage for unique detections and annotations
         unique_text = set()
         annotations = []
 
         while True:
-            # Capture frame-by-frame
             ret, frame = cap.read()
 
             if not ret:
                 break
 
-            # Define current frame
             current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
 
-            # Perform object detection on the frame
             results = model(frame)
 
-            # Process detections and collect text
             current_text = ""
             frame_annotations = []
 
@@ -100,18 +85,14 @@ def convert_video_to_text(video_path):
                 "annotations": frame_annotations
             })
 
-            # Add unique text to set
             unique_text.add(current_text)
 
-            # Print progress
             progress = (current_frame / total_frames) * 100
             print(f"Progress: {progress:.2f}%)")
 
-        # Release everything when done
         cap.release()
         cv2.destroyAllWindows()
 
-        # Return unique text found in the video and the annotations
         return ''.join(unique_text), annotations
     except Exception as e:
         return f"Error processing video: {str(e)}", []
--- Sign_Language_Converter/backend/views.py
+++ Sign_Language_Converter/backend/views.py
@@ -3,35 +3,27 @@ from rest_framework.decorators import api_view
 from .media_processing import convert_image_to_text, convert_video_to_text
 import os
 
-# Upload directory
 UPLOAD_DIR = 'media/uploads/'
 
-# Handle POST request by frontend and return the annotations
+# Handle POST request by frontend and return the annotations and delete the media after this
 @api_view(['POST'])
 def upload_media(request):
 
-    # Check if media is present
     if 'media' not in request.FILES:
         return JsonResponse({'error': 'No media file provided.'}, status=400)
 
-    # Define media file with file extension and filename
     media_file = request.FILES['media']
     file_extension = os.path.splitext(media_file.name)[1].lower()
-    file_name = os.path.splitext(media_file.name)[0]
 
-    # Create the upload path for the media file
     upload_path = os.path.join(UPLOAD_DIR, media_file.name)
 
     try:
-        # Ensure the upload and processed directories exist
         os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-        # Save the uploaded file to UPLOAD_DIR
         with open(upload_path, 'wb') as f:
             for chunk in media_file.chunks():
                 f.write(chunk)
 
-        # Process media file according to thier type
         if file_extension in ['.jpg', '.jpeg', '.png', '.bmp', '.gif']:
             detected_text, annotations = convert_image_to_text(upload_path)
             media_type = 'image'
@@ -41,16 +33,12 @@ def upload_media(request):
         else:
             return JsonResponse({'error': 'Unsupported file type.'})
 
-        # Delete the file after processing is done
         if os.path.exists(upload_path):
             os.remove(upload_path)
 
-        # Return our annotations if everything goes correctly
         return JsonResponse({'message': 'Media processed successfully.', 'text': detected_text, 'annotations': annotations, 'media_type': media_type})
 
     except Exception as e:
-        # If any error occurs, delete the file if it was saved
         if os.path.exists(upload_path):
             os.remove(upload_path)
-        # Return error
         return JsonResponse({'error': str(e)}, status=500)
